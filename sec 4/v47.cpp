e9933ef96f270d683f3a0746ddd487fe884d8432
Gaurav <teachnicalgaurav45@gmail.com>
2026-05-26 22:07:26 +0530

    my commit

---
+++ sec 4/v47.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void moveallNegToleft(int*a, int n) {
+    // Dutch national flag algo'
+    int l=0, h=n-1;
+    while(l<h) {
+        if(a[l] < 0) {
+            l++;
+        }
+        else if(a[h]>0) {
+            h--;
+        }
+        else{
+            swap(a[l], a[h]);
+        }
+    }
+}
+
+
+int main()
+{
+    int a[] = {1, 2, -3, 4, -5, 6};
+    int n = sizeof(a)/sizeof(int);
+    
+    moveallNegToleft(a , n);
+
+    for(int i=0; i<n; i++) {
+        cout<<a[i] << " ";
+    }
+    return 0;
+}
