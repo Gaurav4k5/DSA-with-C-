983b13f8df45106ce213cdaf2b6857a7621686fb
Gaurav <teachnicalgaurav45@gmail.com>
2026-05-30 23:23:36 +0530

    my commit

---
+++ sec 4/v51.cpp
@@ -0,0 +1,36 @@
+// geekforgeeks problems 1132 
+
+#include<vector>
+#include<set>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+        // code here
+        vector<int>ans;
+        set<int>st;
+        int i,j,k;
+        i=j=k=0;
+        while(i<a.size() && j<b.size() && k<c.size() ) {
+            if(a[i] == b[j] && b[j] == c[k]) {
+                st.insert(a[i]);
+                i++,j++,k++;
+            }
+            else if (a[i] < b[j]) {
+                i++;
+            }
+            else if( b[j] < c[k]) {
+                j++;
+            }
+            else{
+                k++;
+            }
+        }
+        for(auto i:st) {
+            ans.push_back(i);
+            
+        }
+        return ans;
+    }
+};
